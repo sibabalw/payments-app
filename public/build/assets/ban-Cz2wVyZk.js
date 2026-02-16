e131456cbb5de071bf747595d3c72f9df4be0d09
Qhayiya Sbabalwe Ntoyi <sibabalwe.ntoyi@appimate.com>
2026-02-16 11:48:57 +0200

    stashed changes

---
+++ public/build/assets/ban-Cz2wVyZk.js
@@ -0,0 +1,1 @@
+import{a as c}from"./createLucideIcon-CDiNb0OM.js";const a=[["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}],["path",{d:"m4.9 4.9 14.2 14.2",key:"1m5liu"}]],o=c("Ban",a);export{o as B};
